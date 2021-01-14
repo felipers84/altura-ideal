168578c8b3bafedb055021823f1c75345a68d4f8
Felipe Campos <felipecampos@MBP-de-Felipe.lan>
2021-01-14 09:32:33 -0400

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String display = "";
+
+  pressionouDigito(String digito) {
+    if (digito.length < 3) {
+      setState(() => display = "${display}${digito}");
+    }
+  }
+
+  limparDigito() {
+    if (display.length >= 1) {
+      setState(() => display = display.substring(0, display.length - 1));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,56 +66,63 @@ class _MyHomePageState extends State<MyHomePage> {
                     boxShape: NeumorphicBoxShape.roundRect(
                         BorderRadius.all(Radius.circular(24)))),
                 child: Padding(
-                  padding: const EdgeInsets.all(16),
+                  padding: const EdgeInsets.all(32),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 24),
-                        child: Neumorphic(
-                          child: Padding(
-                            padding: const EdgeInsets.all(28),
-                            child: Text(
-                              "123",
-                              textAlign: TextAlign.right,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(fontSize: 32),
-                            ),
+                      Neumorphic(
+                        child: Padding(
+                          padding: const EdgeInsets.all(28),
+                          child: Text(
+                            display == "" ? "Informe o peso" : display,
+                            textAlign: TextAlign.right,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                .copyWith(fontSize: 32),
                           ),
-                          style: NeumorphicStyle(
-                              intensity: 2,
-                              depth: -16,
-                              color: Color(0xFF77AA99)),
                         ),
+                        style: NeumorphicStyle(
+                            intensity: 2,
+                            depth: -16,
+                            color: Color(0xFF77AA99),
+                            boxShape: NeumorphicBoxShape.roundRect(
+                                BorderRadius.all(Radius.circular(6)))),
                       ),
                       SizedBox(height: 24),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          BotaoCalculadora(digito: 7),
-                          BotaoCalculadora(digito: 8),
-                          BotaoCalculadora(digito: 9)
+                          BotaoCalculadora(
+                              digito: "<",
+                              pressionouTecla: () => limparDigito())
                         ],
                       ),
+                      SizedBox(height: 12),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          BotaoCalculadora(digito: 4),
-                          BotaoCalculadora(digito: 5),
-                          BotaoCalculadora(digito: 6)
-                        ],
-                      ),
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: ["7", "8", "9"]
+                              .map((e) => BotaoCalculadora(
+                                  digito: e,
+                                  pressionouTecla: () => pressionouDigito(e)))
+                              .toList()),
+                      SizedBox(height: 12),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          BotaoCalculadora(digito: 1),
-                          BotaoCalculadora(digito: 2),
-                          BotaoCalculadora(digito: 3)
-                        ],
-                      )
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: ["4", "5", "6"]
+                              .map((e) => BotaoCalculadora(
+                                  digito: e,
+                                  pressionouTecla: () => pressionouDigito(e)))
+                              .toList()),
+                      SizedBox(height: 12),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: ["1", "2", "3"]
+                              .map((e) => BotaoCalculadora(
+                                  digito: e,
+                                  pressionouTecla: () => pressionouDigito(e)))
+                              .toList()),
                     ],
                   ),
                 ),
@@ -119,21 +140,17 @@ class BotaoCalculadora extends StatelessWidget {
   const BotaoCalculadora({Key key, this.digito, this.pressionouTecla})
       : super(key: key);
 
-  final int digito;
+  final String digito;
   final Function() pressionouTecla;
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: NeumorphicButton(
-            style: NeumorphicStyle(
-                depth: 8, boxShape: NeumorphicBoxShape.circle()),
-            onPressed: () {
-              if (pressionouTecla != null) pressionouTecla();
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(4),
-              child: Text(digito.toString()),
-            )),
-      );
+  Widget build(BuildContext context) => NeumorphicButton(
+      style: NeumorphicStyle(depth: 8, boxShape: NeumorphicBoxShape.circle()),
+      onPressed: () {
+        if (pressionouTecla != null) pressionouTecla();
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: Text(digito),
+      ));
 }
