71cc69cb6471e801ae327cddbcbd0025c3d8a6b7
Felipe Campos <felipecampos@MBP-de-Felipe.lan>
2021-01-16 14:23:26 -0400

    Remoção efeito animado de neumorfismo para melhor performance

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -22,7 +22,6 @@ class Calculadora extends StatefulWidget {
 
 class _Calculadora extends State<Calculadora> with TickerProviderStateMixin {
   String display = "";
-  AnimationController depthController;
   AnimationController opacityController;
 
   @override
@@ -30,13 +29,10 @@ class _Calculadora extends State<Calculadora> with TickerProviderStateMixin {
     opacityController = AnimationController(
         duration: Duration(milliseconds: 500), value: 0, vsync: this)
       ..animateTo(1, curve: Curves.easeInOut);
-    depthController = AnimationController(
-        duration: Duration(milliseconds: 800), value: 0, vsync: this)
-      ..animateTo(1, curve: Curves.easeInOut);
   }
 
   close() {
-    depthController
+    opacityController
         .animateTo(0, duration: Duration(milliseconds: 300))
         .then((value) {
       if (widget.valorInformado != null)
@@ -58,138 +54,128 @@ class _Calculadora extends State<Calculadora> with TickerProviderStateMixin {
   }
 
   @override
-  Widget build(BuildContext context) => AnimatedBuilder(
-        animation: depthController,
-        builder: (context, _) => LayoutBuilder(
-          builder: (context, constraints) => Center(
-            child: Opacity(
-              opacity: opacityController.value,
-              child: Neumorphic(
-                style: NeumorphicStyle(
-                    shape: NeumorphicShape.concave,
-                    depth: 12 * depthController.value,
-                    boxShape: NeumorphicBoxShape.roundRect(
-                        BorderRadius.all(Radius.circular(24)))),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(32, 12, 32, 24),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (context, constraints) => Center(
+          child: Opacity(
+            opacity: opacityController.value,
+            child: Neumorphic(
+              style: NeumorphicStyle(
+                  shape: NeumorphicShape.concave,
+                  depth: 12,
+                  boxShape: NeumorphicBoxShape.roundRect(
+                      BorderRadius.all(Radius.circular(24)))),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(32, 12, 32, 24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("ALTURA IDEAL",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                color: Color(0xFF999999),
+                                fontSize: 12,
+                                fontStyle: FontStyle.italic)),
+                        SizedBox(width: 8),
+                        Image.asset('assets/icone.png', width: 24),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Neumorphic(
+                      child: Stack(
+                        alignment: Alignment.center,
                         children: [
-                          Text("ALTURA IDEAL",
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w900,
-                                  color: Color(0xFF999999),
-                                  fontSize: 12,
-                                  fontStyle: FontStyle.italic)),
-                          SizedBox(width: 8),
-                          Image.asset('assets/icone.png', width: 24),
-                        ],
-                      ),
-                      SizedBox(height: 8),
-                      Neumorphic(
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Container(
-                              height: 100,
-                              child: Center(
-                                child: Text(
-                                  display == "" ? widget.placeholder : display,
-                                  textAlign: TextAlign.right,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      .copyWith(
-                                          fontSize: display == "" ? 24 : 48),
-                                ),
+                          Container(
+                            height: 100,
+                            child: Center(
+                              child: Text(
+                                display == "" ? widget.placeholder : display,
+                                textAlign: TextAlign.right,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1
+                                    .copyWith(
+                                        fontSize: display == "" ? 24 : 48),
                               ),
                             ),
-                            display == ""
-                                ? Container()
-                                : Positioned(
-                                    bottom: 14,
-                                    child: Text(
-                                      widget.unidade,
-                                      style: TextStyle(
-                                          color: Color(0xFF333333),
-                                          fontSize: 12),
-                                    ),
-                                  )
-                          ],
-                        ),
-                        style: NeumorphicStyle(
-                            intensity: 2,
-                            depth: -16 * depthController.value,
-                            color: Color(0xFF77AA99),
-                            boxShape: NeumorphicBoxShape.roundRect(
-                                BorderRadius.all(Radius.circular(6)))),
-                      ),
-                      SizedBox(height: 24),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          BotaoCalculadora(
-                              digito: "<",
-                              pressionouTecla: () => limparDigito(),
-                              animationPosition: depthController.value)
+                          ),
+                          display == ""
+                              ? Container()
+                              : Positioned(
+                                  bottom: 14,
+                                  child: Text(
+                                    widget.unidade,
+                                    style: TextStyle(
+                                        color: Color(0xFF333333), fontSize: 12),
+                                  ),
+                                )
                         ],
                       ),
-                      SizedBox(height: 12),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: ["7", "8", "9"]
-                              .map((e) => BotaoCalculadora(
-                                  digito: e,
-                                  pressionouTecla: () => pressionouDigito(e),
-                                  animationPosition: depthController.value))
-                              .toList()),
-                      SizedBox(height: 12),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: ["4", "5", "6"]
-                              .map((e) => BotaoCalculadora(
-                                  digito: e,
-                                  pressionouTecla: () => pressionouDigito(e),
-                                  animationPosition: depthController.value))
-                              .toList()),
-                      SizedBox(height: 12),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: ["1", "2", "3"]
-                              .map((e) => BotaoCalculadora(
-                                  digito: e,
-                                  pressionouTecla: () => pressionouDigito(e),
-                                  animationPosition: depthController.value))
-                              .toList()),
-                      SizedBox(height: 12),
-                      BotaoCalculadora(
-                          digito: "0",
-                          pressionouTecla: () => pressionouDigito("0"),
-                          animationPosition: depthController.value),
-                      SizedBox(height: 24),
-                      NeumorphicButton(
-                        style: NeumorphicStyle(
-                            depth: 12 * depthController.value,
-                            boxShape: NeumorphicBoxShape.roundRect(
-                                BorderRadius.circular(8))),
-                        onPressed: () {
-                          if (int.tryParse(display) != null) close();
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            "PROSSEGUIR",
-                            style: TextStyle(fontWeight: FontWeight.w800),
-                            textAlign: TextAlign.center,
-                          ),
+                      style: NeumorphicStyle(
+                          intensity: 2,
+                          depth: -16,
+                          color: Color(0xFF77AA99),
+                          boxShape: NeumorphicBoxShape.roundRect(
+                              BorderRadius.all(Radius.circular(6)))),
+                    ),
+                    SizedBox(height: 24),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        BotaoCalculadora(
+                            digito: "<", pressionouTecla: () => limparDigito())
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: ["7", "8", "9"]
+                            .map((e) => BotaoCalculadora(
+                                digito: e,
+                                pressionouTecla: () => pressionouDigito(e)))
+                            .toList()),
+                    SizedBox(height: 12),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: ["4", "5", "6"]
+                            .map((e) => BotaoCalculadora(
+                                digito: e,
+                                pressionouTecla: () => pressionouDigito(e)))
+                            .toList()),
+                    SizedBox(height: 12),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: ["1", "2", "3"]
+                            .map((e) => BotaoCalculadora(
+                                digito: e,
+                                pressionouTecla: () => pressionouDigito(e)))
+                            .toList()),
+                    SizedBox(height: 12),
+                    BotaoCalculadora(
+                        digito: "0",
+                        pressionouTecla: () => pressionouDigito("0")),
+                    SizedBox(height: 24),
+                    NeumorphicButton(
+                      style: NeumorphicStyle(
+                          depth: 12,
+                          boxShape: NeumorphicBoxShape.roundRect(
+                              BorderRadius.circular(8))),
+                      onPressed: () {
+                        if (int.tryParse(display) != null) close();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "PROSSEGUIR",
+                          style: TextStyle(fontWeight: FontWeight.w800),
+                          textAlign: TextAlign.center,
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
@@ -200,18 +186,16 @@ class _Calculadora extends State<Calculadora> with TickerProviderStateMixin {
 }
 
 class BotaoCalculadora extends StatelessWidget {
-  const BotaoCalculadora(
-      {Key key, this.digito, this.pressionouTecla, this.animationPosition})
+  const BotaoCalculadora({Key key, this.digito, this.pressionouTecla})
       : super(key: key);
 
   final String digito;
   final Function() pressionouTecla;
-  final double animationPosition;
 
   @override
   Widget build(BuildContext context) => NeumorphicButton(
       style: NeumorphicStyle(
-          depth: 8 * animationPosition,
+          depth: 8,
           boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(10.0))),
       onPressed: () {
         if (pressionouTecla != null) pressionouTecla();
